db94e4e1c748caa0ffb90d90c3520dd1a3a5e640
Anon <anon@Anon.Dlink>
2018-11-28 19:03:16 +0700

    First commit

---
+++ downloads_cleaning.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3 
+
+import zipfile
+import shutil 
+import os
+
+path = '/Users/Anon/Downloads'
+os.chdir(path)
+
+for filename in os.listdir():
+    if filename.endswith('.torrent') or filename.endswith('.dmg'):
+        os.unlink(filename) print('%s is deleted' % filename)
+    elif filename.endswith('.zip'):
+        try:
+            zipf = zipfile.ZipFile(filename)
+            zipf.extractall(filename[:-4])
+            os.unlink(filename)
+            print('%s was extracted' % filename)
+        except zipfile.BadZipFile:
+            pass
